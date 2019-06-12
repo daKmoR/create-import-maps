262fc7642f6a2092e6cd6a610ff66b3e88a98ffe
thepassle <pascalschilp@gmail.com>
2019-06-12 22:35:05 +0200

    feat: add injectToHtmlFile functionality

---
+++ src/injectToHtmlFile.js
@@ -0,0 +1,17 @@
+import fs from 'fs';
+import path from 'path';
+
+/**
+ * Injects the importmap to the bottom of the body of a html file if specified.
+ * @param {String} fileName
+ * @param {String} importMap
+ */
+export default function injectToHtmlFile(fileName, importMap) {
+  if(!fileName.endsWith('.html')) {
+    console.log('Please enter a valid .html file.');
+  } else {
+    let htmlFile = fs.readFileSync(path.join(process.cwd(), path.sep, fileName), 'utf-8');
+    htmlFile = htmlFile.replace('</body>', `<script type="importmap">${importMap}</script></body>`);
+    fs.writeFileSync(path.join(process.cwd(), path.sep, fileName), htmlFile, 'utf-8');
+  }
+}
