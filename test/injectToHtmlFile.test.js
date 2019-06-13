a187867d052aad844b7af50d984011700b36f46b
thepassle <pascalschilp@gmail.com>
2019-06-14 00:16:56 +0200

    refactor: clean up test

--- test/injectToHtmlFile.test.js
+++ test/injectToHtmlFile.test.js
@@ -8,22 +8,23 @@ const { expect } = chai;
 const importMap = JSON.stringify({"imports": { "foo": "bar"}});
 const indexHtmlPath = `${__dirname}/assets/exampleInjectToHtmlFile/index.html`;
 const indexHtmlWithImportMapPath = `${__dirname}/assets/exampleInjectToHtmlFile/indexWithImportMap.html`;
+const testFilePath = '/test/assets/exampleInjectToHtmlFile';
 
 describe("injectToHtmlFile", () => {
   afterEach(() => {
-    fs.writeFileSync(`${__dirname}/assets/exampleInjectToHtmlFile/index.html`, indexHtml, 'utf-8');
-    fs.writeFileSync(`${__dirname}/assets/exampleInjectToHtmlFile/indexWithImportMap.html`, indexHtmlWithImportMap, 'utf-8');
+    fs.writeFileSync(indexHtmlPath, indexHtml, 'utf-8');
+    fs.writeFileSync(indexHtmlWithImportMapPath, indexHtmlWithImportMap, 'utf-8');
   });
 
   it("injects an importmap in a html file", () => {
-    injectToHtmlFile('/test/assets/exampleInjectToHtmlFile/index.html', importMap);
+    injectToHtmlFile(`${testFilePath}/index.html`, importMap);
     const result = fs.readFileSync(indexHtmlPath).toString();
     const importMapFromHtml = result.match(/<script type="importmap">(.|\n)*?<\/script>/)[0];
     expect(importMapFromHtml).to.equal(`<script type="importmap">${importMap}</script>`);
   });
 
   it("replaces an importmap if one already exists", () => {
-    injectToHtmlFile('/test/assets/exampleInjectToHtmlFile/indexWithImportMap.html', importMap);
+    injectToHtmlFile(`${testFilePath}/indexWithImportMap.html`, importMap);
     const result = fs.readFileSync(indexHtmlWithImportMapPath).toString();
     const importMapFromHtml = result.match(/<script type="importmap">(.|\n)*?<\/script>/)[0];
     expect(importMapFromHtml).to.equal(`<script type="importmap">${importMap}</script>`);
@@ -31,7 +32,7 @@ describe("injectToHtmlFile", () => {
 
   it("logs a message if an incorrect file is input", () => {
     const consoleLogSpy = sinon.spy(console, 'log');
-    injectToHtmlFile('/test/assets/exampleInjectToHtmlFile/incorrectFile.js', importMap);
+    injectToHtmlFile(`${testFilePath}/incorrectFile.js`, importMap);
     expect(consoleLogSpy.calledWith("Please enter a valid .html file.")).to.equal(true);
     consoleLogSpy.restore();
   });
