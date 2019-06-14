d391da41bf17b5139bb8863d4093aad7e900678f
thepassle <pascalschilp@gmail.com>
2019-06-14 21:12:29 +0200

    feat: implement apply overrides

--- src/generateFromYarnLock.js
+++ src/generateFromYarnLock.js
@@ -97,6 +97,40 @@ export async function resolvePathsAndConflicts(
   return resolvedDeps;
 }
 
+function processResolutionName(res) {
+  if(res.includes('node_modules')) return res.split('/node_modules/')[1].split('/')[0];
+  return res;
+ }
+
+export function applyOverrides(imports, packageJson) {
+  const importmap = imports;
+  if(typeof packageJson.importmap !== 'undefined') {
+    const { overrides } = packageJson.importmap;
+
+    Object.keys(overrides).forEach(dep => {
+      if(Array.isArray(overrides[dep])) {
+        /**
+         * we need to delete first, if someone has kv-storage-polyfill in their dependencies, they
+         * should have "std:kv-storage" in their importmap, not "kv-storage-polyfill"
+         */
+        overrides[dep].forEach(res => {
+          const resolution = processResolutionName(res);
+
+          delete importmap[resolution];
+          delete importmap[`${resolution}/`];
+        });
+        importmap[dep] = overrides[dep];
+      } else {
+        delete importmap[dep];
+        delete importmap[`${dep}/`];
+
+        importmap[dep] = overrides[dep];
+      }
+    });
+  }
+  return importmap;
+}
+
 export async function generateFromYarnLock(
   yarnLockString,
   packageJson,
@@ -127,9 +161,12 @@ export async function generateFromYarnLock(
     targetPath
   );
 
-  const imports = flatResolvedDepsToImports(
+  let imports = flatResolvedDepsToImports(
     flatResolvedDepsWithWorkspaceDeps,
     targetPath
   );
+
+  imports = applyOverrides(imports, packageJson);
+
   return { imports };
 }
