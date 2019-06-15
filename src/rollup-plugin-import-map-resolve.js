d309111b4b385e895926e6da0c2c4d7b51e168a1
Lars den Bakker <larsdenbakker@gmail.com>
2019-06-15 08:58:08 +0200

    add docs

--- src/rollup-plugin-import-map-resolve.js
+++ src/rollup-plugin-import-map-resolve.js
@@ -6,10 +6,12 @@ module.exports = function importMapResolvePlugin(importMap, { rootDir = process.
 
   return {
     resolveId(id) {
+      // simple 1on1 mapping ("lit-html": "/node_modules/lit-html/lit-html.js")
       if (keys.includes(id)) {
         return path.join(rootDir, imports[id]);
       }
 
+      // partial mapping ("lit-html/": "/node_modules/lit-html/")
       const partialKey = keys.find(k => k.endsWith('/') && id.startsWith(k));
       if (partialKey) {
         const withoutPrefix = id.replace(partialKey, '');
